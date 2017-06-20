2f6c46bf813f1f2e82d6ae8379c63ee7420af485
Vastrix <>
2017-06-20 21:39:21 +0200

    Realized that there's something call BillingType in steamkit which defines what I want
    HOWEVER, due the lack of documentation regarding steamkit, I'm gonna put this on hold I think
    I'd have to reverse engineer the bloody thing.. :/
    
    https://github.com/SteamDatabase/SteamDatabaseBackend This is what needs to be RE
    https://github.com/SteamRE/SteamKit/blob/master/Resources/SteamLanguage/enums.steamd#L795-L813 This is the enumfile defining the different billingtypes
    https://steamdb.info/sub/182861/info/

---
+++ TempFreeToOwn/HTMLSteamStoreParser.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup
+from colorama import init, Fore, Style
+from urllib import request, response
+init()
+
+def Error(errorMsg):
+    print(Fore.RED + errorMsg + Style.RESET_ALL)
+
+class HTMLSteamStoreParser:
+    """Parses the Steam Store and returns the data in a json"""
+
+    BaseLink = "http://store.steampowered.com/app/"
+
+    def __init__(self, appID: int):
+        if(type(appID) != int): raise Exception("Incorrect appID Type!")
+
+        self.AppID = appID
+        self.CorrectID = True
+        self.Exception = False
+        self.Data = {} #Name, Price, AppID, isGame
+        self.__Soup = None
+
+        if(self.Open()):
+            self.GetInfo()
+
+
+    def Open(self)->bool:
+
+        self.Data["Success"] = False
+
+        try:
+            self.__Soup = request.urlopen(HTMLSteamStoreParser.BaseLink + str(self.AppID))
+            if not(str(self.AppID) in self.__Soup.geturl()): #if the appid is not in the url, assume that there's no game with that ID (we got redirected)
+                self.CorrectID = False
+                return False
+
+            self.__Soup = BeautifulSoup(self.__Soup, "html.parser")
+        except Exception as e:
+            self.__Soup = None
+            self.Exception = True
+
+            Error("[HTMLSSP] An error occured trying to fetch the game site, ID %d"%self.AppID)
+            return False
+        else:
+            self.Data["Success"] = self.__Check()
+            return True
+
+
+    def GetInfo(self) -> bool:
+
+        print(str(self.AppID))
+        if ((self.__Soup is None) or (not(self.Data["Success"]))): return False
+
+
+        self.Data["Name"] = self.__Soup.div.find(attrs={"class": "apphub_AppName"}).text
+
+        self.Data["Price"] = self.__Soup.find(attrs={"itemprop": "price"})["content"] if (self.__Soup.find(attrs={"itemprop": "price"}) is not None) else "NA"
+
+        self.Data["AppID"] = self.AppID
+        self.Data["isGame"] = ("game" in self.__Soup.find(attrs={"class": "blockbg"}).a.text.lower())
+        return True
+
+    def __Check(self)-> bool:
+        """Checks if the currently loaded soup is valid, returns false if not"""
+
+        #Title Check..
+        title = ["error"]
+        for x in title:
+            if (x in self.__Soup.title.text.lower()):
+                return False
+
+        return True
--- TempFreeToOwn/TempFreeToOwn.py
+++ TempFreeToOwn/TempFreeToOwn.py
@@ -1,22 +1,37 @@
 # -*- coding: utf-8 -*-
-import urllib2
+import urllib.request
 import sys
+sys.path.append("C:\Python36\Lib\site-packages\pysteamkit\protobuf")
 import json
+from HTMLSteamStoreParser import HTMLSteamStoreParser
 from time import sleep
-from colorama import init
-from pysteamkit.steam3.client import SteamClient
+from colorama import init, Fore, Style
+from pysteamkit.steam3 import client
 init()
-#client.steamapps.get_product_info(apps=(440,))
-#print "Response:", client.steamapps.app_cache[440]
+
+
+class SimpleHandler():
+    def handle_message(self, emsg_real, body):
+        pass
+    def try_initialize_connection(self, *args, **kwargs):
+        return True
+
+   
+client = client.SteamClient(SimpleHandler())
+
+client.initialize()
+client.login_anonymous()
+print("Response:", client.steamapps.get_product_info(apps=(620,)))
+# SteamApps caches the json values for apps and packages
+print("Json data:", client.steamapps.app_cache[620])
 
 class TempFreeToOwn:
     """Crawls alls steam games in search for temporary free to own games"""
 
     def __init__(self, blackList):
-        self.blackList = json.load(open(blackList))
+        self.blackList = blackList
         self.currentFreeToOwn = {}
         self.appList = []
-        self.Initialize()
 
         for i in range(10):
             if self.Load():
@@ -27,44 +42,51 @@ class TempFreeToOwn:
             sleep(5)
 
 
- 
-
-    def Initialize(self):
-        class SimpleCallBack:
-            def handle_message(self, emsg_real, body):
-                pass
-            def try_initialize_connection(self, *args, **kwargs):
-                return True
-
-        client = SteamClient(SimpleCallBack())
-        client.initialize()
-        client.login_anonymous()
-
-
     def Load(self):
         """caches the appID list and applies the blacklist"""
 
         try:
             print("Fetching the steam apps list..")
-            con = urllib2.urlopen("http://api.steampowered.com/ISteamApps/GetAppList/v0002/")
+            con = urllib.request.urlopen("http://api.steampowered.com/ISteamApps/GetAppList/v0002/")
             self.appList = json.load(con)["applist"]["apps"]
         except:
             return False
         else:
             print("%d apps loaded. Done!\n\nApplying blacklist.." % len(self.appList))
 
-            count = 0
-            for i in self.appList:
-                for val in self.blackList:
-                    if i["appid"] in self.blackList[val]:
-                        self.appList.remove(i)
-                        count +=1
-            print "Removed %d apps, %d remaining" % (count, len(self.appList))
+            jsn = json.load(open(self.blackList))
+
+            self.appList = [x for x in self.appList if not x["appid"] in [jsn[n][a] for n in jsn if (type(jsn[n]) is type([])) for a in range(len(jsn[n]))]] #If it's in a list in the blacklist, Don't keep it
+
+            print("%d remaining" % (len(self.appList)))
 
             return True
 
 
     def Process(self):
-        pass
+
+        print(Fore.YELLOW + "Crawling Initiated!" + Style.RESET_ALL)
+        for v in self.appList:
+            sleep(.5)
+            #print(Fore.GREEN + str(v["appid"]) + Style.RESET_ALL, end='\r')
+            temp = HTMLSteamStoreParser(v["appid"])
+
+            while(temp.Exception == True):
+                temp.Exception = False
+                temp.Open()
+                sleep(.5)
+
+            print(Fore.MAGENTA + "%d; %s"%(v["appid"], temp.Data["Success"]) + Style.RESET_ALL )
+            if(not(temp.Data["Success"])): continue
+
+            if(not(temp.CorrectID) or (not(temp.Data["isGame"]))): #if the ID is incorrect or it's not a game -> Blacklist
+                jsn = json.load(open(self.blackList))
+                jsn["notAGame"].append(v["appid"])
+                json.dump(jsn, open(self.blackList ,'w'))
+                continue
+
+
 
 test = TempFreeToOwn("blackList.json")
+
+test.Process()
